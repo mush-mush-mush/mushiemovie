4e37e87bc9bb81bf06f99dd7ff731aa1925d4496
mush-mush-mush <shroom.mish12@gmail.com>
2022-04-02 12:26:21 +0800

    add composeEnhancers

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <Provider store={store}>
